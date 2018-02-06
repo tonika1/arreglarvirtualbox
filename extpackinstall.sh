e83a54c089fbe1ae8fe9624c4c3387a37c7490d1
tonika1 <36047512+tonika1@users.noreply.github.com>
2018-02-06 20:07:28 +0100

    Update extpackinstall.sh

--- extpackinstall.sh
+++ extpackinstall.sh
@@ -2,15 +2,19 @@
 version=$(vboxmanage -v | tail -1)
 var1=$(echo $version | cut -d 'r' -f 1)
 var2=$(echo $version | cut -d 'r' -f 2)
+var3=$(echo $version | cut -d '.' -f1 )
 var4=$(echo $version | cut -d '.' -f2 )
-paquete=$(echo virtualbox-$var3.$var4)
+file="Oracle_VM_VirtualBox_Extension_Pack-$var1-$var2.vbox-extpack"
+url1="http://download.virtualbox.org/virtualbox/$var1/$file"
+paquete="virtualbox-$var3.$var4"
+
 if [ $(vboxmanage -v | grep vboxconfig | wc -l) -gt 0 ];then
 	sudo /sbin/vboxconfig
 fi
 if [ $(vboxmanage -v | grep vboxconfig | wc -l) -gt 0 ];then
-	# yes "s" | sudo apt purge virtualbox*
-	# sudo apt install $paquete
-  # quizas estas opciones es mejor hacerlas a mano
+  # quizas estas opciones es mejor hacerlas a mano ya que puede implicar interactividad
+  # yes "s" | sudo apt purge virtualbox*
+  # sudo apt install $paquete
   echo ejecuta estos comando y vuelve a lanzar este script
   echo ---------------------------------------------------
   echo sudo apt purge virtualbox*
@@ -18,8 +22,7 @@ if [ $(vboxmanage -v | grep vboxconfig | wc -l) -gt 0 ];then
   echo ---------------------------------------------------
 fi
 if [ $(vboxmanage -v | grep vboxconfig | wc -l) -eq 0 ];then
-	file="Oracle_VM_VirtualBox_Extension_Pack-$var1-$var2.vbox-extpack"
-	wget http://download.virtualbox.org/virtualbox/$var1/$file -O /tmp/$file
+	wget $url -O /tmp/$file
 	if [ $? -eq 0 ];then
 		sudo VBoxManage extpack uninstall "Oracle VM VirtualBox Extension Pack"
 		yes | sudo VBoxManage extpack install /tmp/$file --replace
